7310a8288a1925d3155f9022f987d6b8bddfe26c
Reed Weichler <rweichler@gmail.com>
2016-07-28 23:22:40 -0700

    fix colors

--- include/printcolors.lua
+++ include/printcolors.lua
@@ -3,14 +3,14 @@ c.RED =         '1;31'
 c.GREEN =       '1;32'
 c.YELLOW =      '1;33'
 c.LIGHT_BLUE =  '1;34'
-c.PURPLE =      '1;35'
+c.PINK =        '1;35'
 c.CYAN =        '1;36'
 
 c.DARK_RED =    '0;31'
 c.DARK_GREEN =  '0;32'
 c.BROWN =       '0;33'
 c.BLUE =        '1;34'
-c.DARK_PURPLE = '0;35'
+c.PURPLE =      '0;35'
 c.DARK_CYAN =   '0;36'
 
 for k,v in pairs(c) do
