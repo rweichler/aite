44109ef26683fba135fa15e6ffda0c01ae7d3b60
Reed Weichler <rweichler@gmail.com>
2016-08-01 12:21:46 -0700

    table.merge

--- include/table.lua
+++ include/table.lua
@@ -2,10 +2,15 @@ function table.merge(...)
     local result = {}
     local len = 0
     for _, t in ipairs({...}) do
-        for i, v in ipairs(t) do
-            result[len + i] = v
+        if type(t) == 'table' then
+            for i, v in ipairs(t) do
+                result[len + i] = v
+            end
+            len = len + #t
+        else
+            result[len + 1] = t
+            len = len + 1
         end
-        len = len + #t
     end
     return result
 end
